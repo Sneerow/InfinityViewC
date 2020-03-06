789d8ffde6705fec83830f2f99b534d97b87ba92
Loup Dallier <loup.dallier@epita.fr>
2020-03-06 19:04:16 +0100

    [Website] V1 finished [Website] Added main page, contacts page, downloads page

---
+++ Website/templates/js/main.js
@@ -0,0 +1,62 @@
+// Loader design
+let loader =  "<div class='ui active dimmer'><div class='ui indeterminate text loader'>Preparing Files</div></div><p></p>";
+
+
+// When loaded
+$(document).ready(() => {
+    $.get("./templates/html/home.html", function(data) {
+        $('.view').empty().append(data);
+
+    })
+});
+
+// Button manager
+
+// Home buttom
+$(document).on("click", ".homeButton", function(event) {
+    console.log('test');
+    event.preventDefault();
+    $(".view").empty().append(loader);
+    $(".active").removeClass('active');
+    $(".active").removeClass('active');
+    $("#homeButton1").addClass("active");
+    $("#homeButton2").addClass("active");
+
+    $.get("./templates/html/home.html", function(data) {
+        $('.view').empty().append(data);
+
+    });
+
+});
+
+// Contacts Button
+$(document).on("click", ".contactsButton", function(event) {
+    console.log('test');
+    event.preventDefault();
+    $(".view").empty().append(loader);
+    $(".active").removeClass('active');
+    $(".active").removeClass('active');
+    $("#contactsButton").addClass("active");
+    
+    $.get("./templates/html/contacts.html", function(data) {
+        $('.view').empty().append(data);
+
+    });
+
+});
+
+// Downloads Button
+$(document).on("click" , ".downloadsButton", function(event) {
+    console.log('test');
+    event.preventDefault();
+    $(".view").empty().append(loader);
+    $(".active").removeClass('active');
+    $(".active").removeClass('active');
+    $("#downloadsButton").addClass("active");
+    
+    $.get("./templates/html/downloads.html", function(data) {
+        $('.view').empty().append(data);
+
+    });
+
+});
