9e95340a94c2cd15c2a5d20b3cf38e6271a8661c
Alois Heloir <alois.heloir@epita.fr>
2020-03-07 07:25:17 -0800

    [InfinityView][TextEditor] Add open, copy, paste, cut, select all, 	Also added shortcut, 	Trying to add option font but deperciated (use css);

--- InfinityView/main.c
+++ InfinityView/main.c
@@ -2,13 +2,22 @@
 #include <string.h>
 
 #include <gtk/gtk.h>
+#include <gdk/gdk.h>
 
 struct menu_bar {
 	GtkWidget *new;
 	GtkWidget *open;
 	GtkWidget *save;
 	GtkWidget *saveas;
+	GtkWidget *close;
 	GtkWidget *quit;
+	
+	GtkWidget *cut;
+	GtkWidget *copy;
+	GtkWidget *paste;
+	GtkWidget *select_all;
+
+	GtkWidget *font;
 };
 
 struct text_editor {
@@ -22,11 +31,22 @@ struct text_editor {
 // Init functions
 void init_menu_bar(struct menu_bar *menubar, GtkBuilder *builder) 
 {
+	// File Item
 	menubar->new = GTK_WIDGET(gtk_builder_get_object(builder, "new_menuItem"));	
 	menubar->open = GTK_WIDGET(gtk_builder_get_object(builder, "open_menuItem"));	
 	menubar->save = GTK_WIDGET(gtk_builder_get_object(builder, "save_menuItem"));	
 	menubar->saveas = GTK_WIDGET(gtk_builder_get_object(builder, "saveas_menuItem"));	
+	menubar->close = GTK_WIDGET(gtk_builder_get_object(builder, "close_menuItem"));	
 	menubar->quit = GTK_WIDGET(gtk_builder_get_object(builder, "quit_menuItem"));	
+
+	// Edit Item
+	menubar->cut = GTK_WIDGET(gtk_builder_get_object(builder, "cut_menuItem"));	
+	menubar->copy = GTK_WIDGET(gtk_builder_get_object(builder, "copy_menuItem"));	
+	menubar->paste = GTK_WIDGET(gtk_builder_get_object(builder, "paste_menuItem"));	
+	menubar->select_all = GTK_WIDGET(gtk_builder_get_object(builder, "select_all_menuItem"));
+
+	// Options Item
+	menubar->font = GTK_WIDGET(gtk_builder_get_object(builder, "font_menuItem"));
 }
 
 void init_text_editor(struct text_editor *texteditor, GtkBuilder *builder, GtkWidget *window)
@@ -41,15 +61,33 @@ void init_text_editor(struct text_editor *texteditor, GtkBuilder *builder, GtkWi
 // GTK FUNCTION
 // ============
 
+// ====
+// FILE
+// ====
+
+// Function to create a new file
 void new_file(GtkWidget *widget, struct text_editor *texteditor)
 {
 	gtk_widget_show(widget);
+
 	gtk_widget_show(texteditor->text_view);
 	gtk_widget_show(texteditor->file_label);
 	gtk_label_set_text(GTK_LABEL(texteditor->file_label), "Untitled");
 	gtk_text_buffer_set_text(texteditor->buffer, "", -1);
 }
 
+// Function to close a file
+void close_file(GtkWidget *widget, struct text_editor *texteditor)
+{
+	gtk_widget_show(widget);
+
+	gtk_widget_hide(texteditor->text_view);
+	gtk_widget_hide(texteditor->file_label);
+	gtk_label_set_text(GTK_LABEL(texteditor->file_label), "");
+	gtk_text_buffer_set_text(texteditor->buffer, "", -1);
+}
+
+// Function to quick save a file
 void save_file(GtkWidget *widget, struct text_editor *texteditor)
 {
 	gtk_widget_show(widget);
@@ -100,7 +138,7 @@ void save_file(GtkWidget *widget, struct text_editor *texteditor)
 	}
 }
 
-
+// Function to save a file
 void saveas_file(GtkWidget *widget, struct text_editor *texteditor)
 {
 
@@ -140,6 +178,106 @@ void saveas_file(GtkWidget *widget, struct text_editor *texteditor)
 	gtk_widget_destroy(dialog);
 }
 
+// Function to open a file
+void open_file(GtkWidget *widget, struct text_editor *texteditor)
+{
+	gtk_widget_show(widget);
+
+	GtkWidget *dialog;
+	dialog = gtk_file_chooser_dialog_new("Open File", GTK_WINDOW(texteditor->window), 
+			GTK_FILE_CHOOSER_ACTION_OPEN,
+			"_Open", GTK_RESPONSE_ACCEPT,
+			"_Cancel", GTK_RESPONSE_CANCEL,
+			NULL);
+	gint response;
+	response = gtk_dialog_run(GTK_DIALOG(dialog));
+	
+	if (response == GTK_RESPONSE_ACCEPT)
+	{
+		gtk_widget_show(texteditor->text_view);
+		gtk_widget_show(texteditor->file_label);
+
+		gchar *contents;
+		gchar *filename;
+		filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
+		g_file_get_contents(filename, &contents, NULL, NULL);
+		gtk_text_buffer_set_text(texteditor->buffer, contents, -1);
+		gtk_label_set_text(GTK_LABEL(texteditor->file_label), filename);
+
+
+		g_free(filename);
+	}
+
+	gtk_widget_destroy(dialog);
+}
+
+// ====
+// EDIT
+// ====
+
+void cut_to_clipboard(GtkWidget *widget, GtkTextBuffer *buffer)
+{
+	gtk_widget_show(widget);
+
+	GtkClipboard *clipboard;
+	clipboard = gtk_clipboard_get(GDK_NONE);
+	gtk_text_buffer_cut_clipboard(buffer, clipboard, TRUE);
+}
+
+void copy_to_clipboard(GtkWidget *widget, GtkTextBuffer *buffer)
+{
+	gtk_widget_show(widget);
+
+	GtkClipboard *clipboard;
+	clipboard = gtk_clipboard_get(GDK_NONE);
+	gtk_text_buffer_copy_clipboard(buffer, clipboard);
+}
+
+void paste_to_clipboard(GtkWidget *widget, GtkTextBuffer *buffer)
+{
+	gtk_widget_show(widget);
+
+	GtkClipboard *clipboard;
+	clipboard = gtk_clipboard_get(GDK_NONE);
+	gtk_text_buffer_paste_clipboard(buffer, clipboard, NULL, TRUE);
+}
+
+void select_all_text(GtkWidget *widget, GtkTextBuffer *buffer)
+{
+	gtk_widget_show(widget);
+
+	GtkTextIter start;
+	GtkTextIter end;
+	gtk_text_buffer_get_bounds(buffer, &start, &end);
+	gtk_text_buffer_select_range(buffer, &start, &end);
+
+}
+
+// =======
+// Options 
+// =======
+
+void select_font(GtkWidget *widget, struct text_editor *texteditor)
+{
+	gtk_widget_show(widget);
+
+	GtkWidget *dialog = gtk_font_chooser_dialog_new("Select Font", GTK_WINDOW(texteditor->window));
+	gint result;
+
+	result = gtk_dialog_run(GTK_DIALOG(dialog));
+
+	if (result == GTK_RESPONSE_OK || result == GTK_RESPONSE_APPLY)
+	{
+		PangoFontDescription *font_desc;
+		font_desc = gtk_font_chooser_get_font_desc(GTK_FONT_CHOOSER(dialog));
+
+		gtk_widget_modify_font(GTK_WIDGET(texteditor->text_view), font_desc);
+	}
+
+	gtk_widget_destroy(dialog);
+}
+
+
 int main(int argc, char *argv[])
 {
 	// ========
@@ -161,12 +299,39 @@ int main(int argc, char *argv[])
 	struct text_editor *texteditor = malloc(sizeof(struct text_editor));
 	init_text_editor(texteditor, builder, window);
 
+	// Init shortcut (accel group)
+	GtkAccelGroup *accel_group = NULL;
+	accel_group = gtk_accel_group_new();
+	gtk_window_add_accel_group(GTK_WINDOW(window), accel_group);
+
+	// Add shortcut
+	
+	gtk_widget_add_accelerator(menubar->new, "activate", accel_group, GDK_KEY_n, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
+	gtk_widget_add_accelerator(menubar->open, "activate", accel_group, GDK_KEY_o, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
+	gtk_widget_add_accelerator(menubar->save, "activate", accel_group, GDK_KEY_s, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
+	gtk_widget_add_accelerator(menubar->close, "activate", accel_group, GDK_KEY_w, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
+	gtk_widget_add_accelerator(menubar->quit, "activate", accel_group, GDK_KEY_q, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
+	
+	gtk_widget_add_accelerator(menubar->copy, "activate", accel_group, GDK_KEY_c, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
+	gtk_widget_add_accelerator(menubar->paste, "activate", accel_group, GDK_KEY_v, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
+	gtk_widget_add_accelerator(menubar->cut, "activate", accel_group, GDK_KEY_x, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
+	gtk_widget_add_accelerator(menubar->select_all, "activate", accel_group, GDK_KEY_a, GDK_CONTROL_MASK, GTK_ACCEL_VISIBLE);
+
 	// Connect Signal
 	g_signal_connect(menubar->quit, "activate", G_CALLBACK(gtk_main_quit), NULL);
 	g_signal_connect(menubar->new, "activate", G_CALLBACK(new_file), texteditor);
 	g_signal_connect(menubar->save, "activate", G_CALLBACK(save_file), texteditor);
 	g_signal_connect(menubar->saveas, "activate", G_CALLBACK(saveas_file), texteditor);
+	g_signal_connect(menubar->open, "activate", G_CALLBACK(open_file), texteditor);
+	g_signal_connect(menubar->close, "activate", G_CALLBACK(close_file), texteditor);
 	
+	g_signal_connect(menubar->copy, "activate", G_CALLBACK(copy_to_clipboard), texteditor->buffer);
+	g_signal_connect(menubar->paste, "activate", G_CALLBACK(paste_to_clipboard), texteditor->buffer);
+	g_signal_connect(menubar->cut, "activate", G_CALLBACK(cut_to_clipboard), texteditor->buffer);
+	g_signal_connect(menubar->select_all, "activate", G_CALLBACK(select_all_text), texteditor->buffer);
+
+	g_signal_connect(menubar->font, "activate", G_CALLBACK(select_font), texteditor);
+
 	// Hidding object
 	gtk_widget_hide(texteditor->text_view);
 	gtk_widget_hide(texteditor->file_label);
